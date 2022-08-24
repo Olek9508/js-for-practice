e901287b6f738d51d3eb6ab62bbfc9e0e05afc26
Oleksandr Volik <polskasasha@gmail.com>
2022-08-25 02:06:47 +0300

    update

--- js/practice.js
+++ js/practice.js
@@ -21,16 +21,20 @@
 
 // console.log(aBC);
 
-function includes(array, value) {
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-}
+// function includes(array, value) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i] === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(
-  includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus'),
-);
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(
+//   includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus'),
+// );
+
+// (name => {
+//   console.log(name);
+// })('Mango');

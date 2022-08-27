32f716338e56136df35ecc0361c0eebc604746fa
Oleksandr Volik <polskasasha@gmail.com>
2022-08-27 04:01:15 +0300

    some-features

--- src/js/open-home-page.js
+++ src/js/open-home-page.js
@@ -3,35 +3,36 @@ import { renderTrendingMovies } from './renderTrendingMovies';
 import { renderButtonsOfPagination } from './pagination';
 import { selectPage } from './pagination';
 import { onHomeClick } from './header';
-import { openModalWindow } from './modal'
-import { openLibrary } from './open-library'
+import { openModalWindow } from './modal';
+import { openLibrary } from './open-library';
 import { toggleTheme } from './light-theme-switcher';
+import { onCustomSearch } from './search-by-keyword';
 // import { openMovieDetails } from './open-movie-details'
 
 const LIBRARY = document.querySelector('#library');
 const HOME = document.querySelector('#home');
 // const searchInput = document.querySelector('header-search');
 const input = document.querySelector('#slider');
-const paginationButtons = document.querySelector(".pagination-nav")
+const paginationButtons = document.querySelector('.pagination-nav');
 // const gallery = document.querySelector('.films_list')
-const openBtn = document.querySelector('.footer__authorship')
+const openBtn = document.querySelector('.footer__authorship');
 
 // searchInput.addEventListener('submit', getInputValue)
 HOME.addEventListener('click', openHomePage);
 LIBRARY.addEventListener('click', openLibrary);
 input.addEventListener('click', toggleTheme);
 // gallery.addEventListener('click', openMovieDetails)
-paginationButtons.addEventListener("click", selectPage)
+paginationButtons.addEventListener('click', selectPage);
 openBtn.addEventListener('click', openModalWindow);
 
-function openHomePage () {
-    getTrendingMovies(1)
+function openHomePage() {
+  getTrendingMovies(1)
     .then(film => {
-        renderTrendingMovies(film.data.results)
-        renderButtonsOfPagination(film.data, 1)
-        onHomeClick()
+      renderTrendingMovies(film.data.results);
+      renderButtonsOfPagination(film.data, 1);
+      onHomeClick();
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
 }
 
-export { openHomePage }
+export { openHomePage };
--- src/js/search-by-keyword.js
+++ src/js/search-by-keyword.js
@@ -1,1 +1,29 @@
+import { getMovieByKeyword } from './fetchFilms';
+import { openHomePage } from './open-home-page';
+import { renderTrendingMovies } from './renderTrendingMovies';
 
+const searchInput = document.querySelector('.header-search');
+const galleryContainer = document.querySelector('.films_list');
+// const films = film.data.results;
+
+searchInput.addEventListener('submit', onCustomSearch);
+
+let formValue = null;
+let currentPage = 1;
+
+function onCustomSearch(event) {
+  event.preventDefault();
+  formValue = event.target.query.value.toLowerCase().trim();
+  console.log(formValue);
+  if (!formValue) {
+    galleryContainer.innerHTML = '';
+    console.log('no result');
+    return;
+  } else {
+    galleryContainer.innerHTML = '';
+    console.log('bad request');
+    return;
+  }
+}
+
+export { onCustomSearch };

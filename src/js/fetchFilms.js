966d291b99531e54d24cd3ae54df2296623d0c43
Oleksandr Volik <polskasasha@gmail.com>
2022-08-28 15:44:44 +0300

    add input function

--- src/js/fetchFilms.js
+++ src/js/fetchFilms.js
@@ -1,29 +1,37 @@
-import axios from "axios"
+import axios from 'axios';
 
-const KEY = '2acc48bc8101b89794229029120e4b70'
-const BASE_URL = 'https://api.themoviedb.org/3/'
+const KEY = '2acc48bc8101b89794229029120e4b70';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
-async function getTrendingMovies (page) {
-    const response = await axios.get(`${BASE_URL}trending/movie/week?api_key=${KEY}&page=${page}`)
-    return response
+async function getTrendingMovies(page) {
+  const response = await axios.get(
+    `${BASE_URL}trending/movie/week?api_key=${KEY}&page=${page}`
+  );
+  return response;
 }
 
-async function getMovieByKeyword (keyword, page) {
-    const response = await axios.get(`${BASE_URL}search/movie?api_key=${KEY}&query=${keyword}&page=${page}`)
-    return response
+async function getMovieByKeyword(keyword, page) {
+  const response = await axios.get(
+    `${BASE_URL}search/movie?api_key=${KEY}&query=${keyword}&page=${page}`
+  );
+  return response;
 }
 
-async function getMovieDetails (movie_id) {
-    const response = await axios.get(`${BASE_URL}movie/${movie_id}?api_key=${KEY}`)
-    return response
+async function getMovieDetails(movie_id) {
+  const response = await axios.get(
+    `${BASE_URL}movie/${movie_id}?api_key=${KEY}`
+  );
+  return response;
 }
 
-async function getMovieGenres () {
-    const response = await axios.get(`${BASE_URL}genre/movie/list?api_key=${KEY}`)
-    return response
+async function getMovieGenres() {
+  const response = await axios.get(
+    `${BASE_URL}genre/movie/list?api_key=${KEY}`
+  );
+  return response;
 }
 
-export { getTrendingMovies }
-export { getMovieByKeyword }
-export { getMovieDetails }
-export { getMovieGenres }
+export { getTrendingMovies };
+export { getMovieByKeyword };
+export { getMovieDetails };
+export { getMovieGenres };
--- src/js/search-by-keyword.js
+++ src/js/search-by-keyword.js
@@ -2,29 +2,32 @@ import { getMovieByKeyword } from './fetchFilms';
 import { renderTrendingMovies } from './renderTrendingMovies';
 import { renderButtonsOfPagination } from './pagination';
 
-const searchValue = document.querySelector('.submit-search');
-const galleryContainer = document.querySelector('.films_list');
+const refs = {
+  searchValue: document.querySelector('.submit-search'),
+  galleryContainer: document.querySelector('.films_list'),
+};
 
-searchValue.addEventListener('submit', onCustomSearch);
+refs.searchValue.addEventListener('submit', onCustomSearch);
 
-let currentPage = 1;
+function generalSettings() {
+  refs.searchValue.reset();
+  refs.galleryContainer.innerHTML = '';
+}
 
 function onCustomSearch(event) {
   event.preventDefault();
   const formValue = event.target.query.value.toLowerCase().trim();
-  console.log(formValue);
-  preloaderAgain();
   if (!formValue) {
-    galleryContainer.innerHTML = '';
+    generalSettings();
     console.log('no result');
     return;
   } else {
-    galleryContainer.innerHTML = '';
-    console.log('bad request');
-    getMovieByKeyword(formValue, currentPage)
+    generalSettings();
+    getMovieByKeyword(formValue, 1)
       .then(film => {
         renderTrendingMovies(film.data.results);
         renderButtonsOfPagination(film.data, 1);
+        console.log(film.data);
       })
       .catch(error => console.log(error));
   }
